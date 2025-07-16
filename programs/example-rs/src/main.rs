4813dd8660f734fff192c4d5c888264201931beb
lmeller-git <louis.meller@icloud.com>
2025-07-16 12:13:13 +0200

    add print

--- programs/example-rs/src/main.rs
+++ programs/example-rs/src/main.rs
@@ -1,11 +1,11 @@
 #![no_std]
 #![no_main]
 
-// extern crate libtinyos;
-
-use libtinyos::exit;
+extern crate alloc;
+use libtinyos::{exit, println};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main() -> ! {
+    println!("hello from userland");
     exit(0);
 }
