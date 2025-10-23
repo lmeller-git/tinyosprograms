6241784a7928873b66a2d3ebc22da34cde73548e
lmeller-git <louis.meller@icloud.com>
2025-10-23 12:11:33 +0200

    update example-c and example-asm

--- programs/example-asm/exit.asm
+++ programs/example-asm/exit.asm
@@ -3,17 +3,17 @@ section .text
 
 _start:
         ; print msg
-        mov rax, 4
-        mov rdi, 1
+        mov rax, 3
+        mov rdi, 0
         mov rsi, msg
         mov rdx, len
         int 0x80
 
         ; exit
-        mov rax, 1
+        mov rax, 5
         mov rdi, 0
         int 0x80
-        
+
 section .data
         msg db "Hello World from example-asm!", 0xa
         len equ $ -msg
