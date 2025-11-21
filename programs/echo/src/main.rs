4fc5d5fbf781d96df0beeae80f8a75a0880cbc04
lmeller-git <louis.meller@icloud.com>
2025-11-21 14:26:10 +0100

    add debug to echo

--- programs/echo/src/main.rs
+++ programs/echo/src/main.rs
@@ -11,7 +11,7 @@ pub extern "C" fn _start(arg: *const u8, _: *const u8, buf_size: usize) -> ! {
             _ = unsafe { syscalls::write(STDERR_FILENO, bytes.as_ptr(), bytes.len()) };
         }
     } else {
-        _ = unsafe { syscalls::write(STDOUT_FILENO, b"".as_ptr(), 0) };
+        _ = unsafe { syscalls::write(STDOUT_FILENO, b"no_arg".as_ptr(), b"no_arg".len()) };
     }
 
     unsafe { syscalls::exit(0) }
